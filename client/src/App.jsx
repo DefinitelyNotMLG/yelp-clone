42df5b2c44e4c14a9b48f7f22bd58bdfde395edc
DefinitelyNotMLG <72935141+DefinitelyNotMLG@users.noreply.github.com>
2020-11-15 09:53:02 -0500

    homepage content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Update from "./routes/Update";
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
+
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <RestaurantsContextProvider>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/restaurants/:id/update" component={Update} />
+                        <Route exact path="/restaurants/:id" component={Detail} />
+                    </Switch>
+                </Router>
+            </div>
+        </RestaurantsContextProvider >
+    )
 };
 
 export default App;
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainHeader from '../components/MainHeader'
+import MainAdd from '../components/MainAdd'
+import MainList from '../components/MainList'
+
+const Home = () => {
+    return (
+        <div>
+            <MainHeader />
+            <MainAdd />
+            <MainList/>
+        </div>
+    )
+}
+
+export default Home
